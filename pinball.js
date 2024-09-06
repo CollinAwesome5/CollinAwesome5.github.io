32a4ea304036d995b80870e985df8df90316bbc8
CollinAwesome5 <159289254+CollinAwesome5@users.noreply.github.com>
2024-09-05 21:06:52 -0400

    Update pinball.js

--- pinball.js
+++ pinball.js
@@ -18,6 +18,7 @@ var ball = {
 var simWidth, simHeight, cScale;
 
 function resizeCanvas() {
+    console.log('resizing canvas');
     canvas.width = window.innerWidth - 20;
     canvas.height = window.innerHeight - 100;
     updateScale();
